74997d8059c50f91e283ac8ccaf04a2fc0fc9158
mradulpython <166733239+mradulpython@users.noreply.github.com>
2024-09-03 23:38:13 +0530

    Calculator

---
+++ Python-WorkShop/calculator.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter First number: ")) 
+num2 = int(input("Enter Second number: ")) 
+
+print(type(num1))
+print(type(num2))
+
+sum_of_num = num1 + num2
+print("Sum of two numbers",sum_of_num)
+
+diff_of_num = num1 - num2
+print("Diff of two numbers",diff_of_num)
+
+prod_of_num = num1 * num2
+print("Prod of two numbers",prod_of_num)
+
+div_of_num = num1 /  num2
+print("Div of two numbers",div_of_num)
